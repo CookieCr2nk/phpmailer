dc73e11a95b2eb93bdf7635b5d942ec1bbae5398
Noa J <38990807+CookieCr2nk@users.noreply.github.com>
2020-10-15 19:21:38 +0200

    Changing Text

--- send.php
+++ send.php
@@ -3,14 +3,16 @@ include("phpmailer/PHPMailerAutoload.php");
 
 //$emailText = $content = file_get_contents('http://loripsum.net/api');
 $emailText = "
-Dual-Stack
+Sehr geehrter Herr Joder
 
-Die Migration zu Dual Stack ist vergleichsweise einfach. Viele Betriebssysteme können mit Dual Stack, also IPv4 und IPv6 gleichzeitig, umgehen. Alle bestehenden Dienste sind weiterhin unter ihrer gewohnten IPv4-Adresse erreichbar. Nach und nach kann man bestehende Dienste per IPv6 erreichbar machen.
-Der Schritt zu IPv6 und damit Dual Stack, wird nur häufig deshalb nicht vollzogen, weil während des Parallelbetriebs der doppelte Administrationsaufwand anfällt. Beispielsweise müssen statische IP-Konfigurationen und das Routing, Filterregeln und Access Control Lists doppelt geführt werden. Und das bedeutet auch, es gibt die doppelte Anzahl an Fehlerquellen.
-Erschwerend kommt hinzu, dass man es in der Regel mit Mitarbeitern, Dienstleistern und Experten zu tun hat, die vor Jahren oder Jahrzehnten IPv4 gelernt und verinnerlicht haben. Die müssen IPv6 völlig neu lernen. Denn IPv4 und IPv6 sind in vielen Dingen vergleichbar, aber nicht identisch.
-Das Problem der Adressknappheit bei IPv4 löst man aber nicht dadurch, dass man IPv6 ignoriert. Irgendwann kommt man an den Punkt, an dem neue Dienste per IPv6 erreichbar sein müssen, weil die Kunden nur noch eine eingeschränkte oder gar keine IPv4-Connectivity mehr haben.
+Das ist eine ausführliche Nachricht die Täglich versand wird, um die Mail Reputation zu steigern. Bitte beachte Stehts, dass hier keine Fluchwörter drin stehen und dass du die Mails regelmässig liest.
+Denn das ist auch wichtig für eine gute Mail reputation.
 
-Alle Lösungswege im Hinblick auf die aktuellen Probleme mit IPv4/IPv6 sollten in Richtung IPv6-only führen. Egal welche Erfahrungen der eine oder andere gemacht hat oder noch machen wird. An IPv6 führt kein Weg vorbei. Auf dem Weg Richtung IPv6-only wird Dual Stack, also der Parallelbetrieb von IPv4 und IPv6, der Normalfall sein. Auf Jahrzehnte gesehen. Wie genau das Dual Stack aussieht, hängt aber vom Provider ab.
+Ich könnte hier noch mehr Text hinballern, aber das lasse ich jetzt erstmal.
+
+See you later aligator
+
+Noa Joder
 ";
 
 $mail = new PHPMailer();
@@ -26,7 +28,7 @@ $mail->isHTML();
 $mail->Username = 'noa@joder.li';
 $mail->Password = 'sax4WdqafDZSXPz9';
 $mail->SetFrom('noa@joder.li', 'Noa Joder');
-$mail->Subject = "Noa Joder - Road to IP Reputation IPv4 Stack";
+$mail->Subject = "Wieso eine gute IP Reputation wichtig ist.";
 $mail->Body = $emailText;
 
 $mail->AddAddress("noa.joder@outlook.com");
