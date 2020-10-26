99fb2cbd45a3530bf8a3d647dafd0925d01b3363
Noa J <38990807+CookieCr2nk@users.noreply.github.com>
2020-10-26 18:10:12 +0100

    Implement "random" Mechanismen

--- send.php
+++ send.php
@@ -1,10 +1,18 @@
 <?php
 include("phpmailer/PHPMailerAutoload.php");
 
-//$content1 = file_get_contents('http://loripsum.net/api');
+//https://stackoverflow.com/questions/8676183/echoing-out-a-random-variable
+//Define different content that fetch from API
+$content1 = file_get_contents('http://loripsum.net/api');
 $content2 = file_get_contents('https://baconipsum.com/api/?type=meat-and-filler&paras=5&format=text');
+$content3 = file_get_contents('https://api.jokes.one/jod?category=animal');
+$content4 = file_get_contents('https://api.jokes.one/jod?category=blonde');
+$content5 = file_get_contents('https://api.jokes.one/jod');
 
-$emailText = $content2;
+//Define Array with content(x)
+$array = array($content1, $content2, $content3, $content4, $content5);
+
+$emailText = $array[rand(0, count($array) - 1)];
 
 $mail = new PHPMailer();
 $mail->isSMTP();
